a6e21b7486a05cd3364b40522e8f28538816c01a
iExploze <ianzhang069@gmail.com>
2026-05-09 14:26:25 -0400

    qs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,164 @@
 using UnityEngine;
 
+public enum PlayerState
+{
+    Normal,
+    Stunned,
+    Finished,
+    Disabled
+}
+
+[System.Serializable]
+public class PlayerStats
+{
+    [Header("Movement")]
+    public float moveSpeed = 8f;
+    public float acceleration = 45f;
+    public float deceleration = 55f;
+    public float airControl = 0.55f;
+
+    [Header("Jumping")]
+    public float jumpForce = 8f;
+    public float extraGravity = 18f;
+
+    [Header("Rotation")]
+    public float rotationSpeed = 14f;
+
+    public PlayerStats Clone()
+    {
+        return new PlayerStats
+        {
+            moveSpeed = moveSpeed,
+            acceleration = acceleration,
+            deceleration = deceleration,
+            airControl = airControl,
+            jumpForce = jumpForce,
+            extraGravity = extraGravity,
+            rotationSpeed = rotationSpeed
+        };
+    }
+}
+
+[RequireComponent(typeof(PlayerInputHandler))]
+[RequireComponent(typeof(PlayerMotor))]
+[RequireComponent(typeof(PlayerEffectController))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("References")]
+    [SerializeField] private PlayerInputHandler input;
+    [SerializeField] private PlayerMotor motor;
+    [SerializeField] private PlayerEffectController effects;
+
+    [Header("Child References")]
+    [SerializeField] private Transform visualRoot;
+    [SerializeField] private Transform cameraTarget;
+    [SerializeField] private Transform groundCheck;
+
+    [Header("Stats")]
+    [SerializeField] private PlayerStats baseStats = new PlayerStats();
+
+    public PlayerStats CurrentStats { get; private set; }
+    public PlayerState State { get; private set; } = PlayerState.Normal;
+
+    public int PlayerIndex => input.PlayerIndex;
+    public Transform CameraTarget => cameraTarget;
+    public Transform VisualRoot => visualRoot;
+    public Transform GroundCheck => groundCheck;
+
+    private void Awake()
     {
-        
+        input ??= GetComponent<PlayerInputHandler>();
+        motor ??= GetComponent<PlayerMotor>();
+        effects ??= GetComponent<PlayerEffectController>();
+
+        if (visualRoot == null)
+            visualRoot = transform.Find("VisualRoot");
+
+        if (cameraTarget == null)
+            cameraTarget = transform.Find("CameraTarget");
+
+        if (groundCheck == null)
+            groundCheck = transform.Find("GroundCheck");
+
+        motor.Initialize(this, groundCheck);
+        effects.Initialize(this);
+
+        RecalculateStats();
+        State = PlayerState.Normal;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (State != PlayerState.Normal)
+            return;
+
+        if (input.JumpPressed)
+        {
+            motor.TryJump(CurrentStats);
+            input.ConsumeJump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (State != PlayerState.Normal)
+            return;
+
+        effects.TickEffects(Time.fixedDeltaTime);
+        motor.Move(input.MoveInput, CurrentStats);
+        motor.ApplyExtraGravity(CurrentStats);
+    }
+
+    public void RecalculateStats()
+    {
+        CurrentStats = baseStats.Clone();
+        effects.ModifyStats(CurrentStats);
+    }
+
+    public void SetState(PlayerState newState)
+    {
+        State = newState;
+
+        if (State != PlayerState.Normal)
+        {
+            motor.StopHorizontalMovement();
+        }
+    }
+
+    public void ResetForRound(Vector3 spawnPosition, Quaternion spawnRotation)
+    {
+        transform.SetPositionAndRotation(spawnPosition, spawnRotation);
+
+        motor.ResetVelocity();
+        input.ResetInput();
+
+        State = PlayerState.Normal;
+
+        RecalculateStats();
+        effects.NotifyRoundStart();
+    }
+
+    public void FinishRound()
+    {
+        State = PlayerState.Finished;
+        motor.StopHorizontalMovement();
+        effects.NotifyRoundEnd();
+    }
+
+    public void Stun(float duration)
+    {
+        if (gameObject.activeInHierarchy)
+            StartCoroutine(StunRoutine(duration));
+    }
+
+    private System.Collections.IEnumerator StunRoutine(float duration)
+    {
+        State = PlayerState.Stunned;
+        motor.StopHorizontalMovement();
+
+        yield return new WaitForSeconds(duration);
+
+        if (State == PlayerState.Stunned)
+            State = PlayerState.Normal;
     }
 }
--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -2,15 +2,100 @@ using UnityEngine;
 
 public class PlayerInputHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum ControlPreset
     {
-        
+        PlayerOneWASD,
+        PlayerTwoArrows,
+        Custom
     }
 
-    // Update is called once per frame
-    void Update()
+    [Header("Player")]
+    [SerializeField] private int playerIndex = 0;
+    [SerializeField] private ControlPreset controlPreset = ControlPreset.PlayerOneWASD;
+
+    [Header("Custom Keys")]
+    [SerializeField] private KeyCode moveUp = KeyCode.W;
+    [SerializeField] private KeyCode moveDown = KeyCode.S;
+    [SerializeField] private KeyCode moveLeft = KeyCode.A;
+    [SerializeField] private KeyCode moveRight = KeyCode.D;
+    [SerializeField] private KeyCode jump = KeyCode.Space;
+
+    public int PlayerIndex => playerIndex;
+    public Vector2 MoveInput { get; private set; }
+    public bool JumpPressed { get; private set; }
+
+    private void Awake()
     {
-        
+        ApplyPreset();
+    }
+
+    private void Update()
+    {
+        ReadMovement();
+        ReadJump();
+    }
+
+    private void ApplyPreset()
+    {
+        if (controlPreset == ControlPreset.PlayerOneWASD)
+        {
+            playerIndex = 0;
+
+            moveUp = KeyCode.W;
+            moveDown = KeyCode.S;
+            moveLeft = KeyCode.A;
+            moveRight = KeyCode.D;
+            jump = KeyCode.Space;
+        }
+        else if (controlPreset == ControlPreset.PlayerTwoArrows)
+        {
+            playerIndex = 1;
+
+            moveUp = KeyCode.UpArrow;
+            moveDown = KeyCode.DownArrow;
+            moveLeft = KeyCode.LeftArrow;
+            moveRight = KeyCode.RightArrow;
+            jump = KeyCode.RightShift;
+        }
+    }
+
+    private void ReadMovement()
+    {
+        float x = 0f;
+        float y = 0f;
+
+        if (Input.GetKey(moveLeft))
+            x -= 1f;
+
+        if (Input.GetKey(moveRight))
+            x += 1f;
+
+        if (Input.GetKey(moveDown))
+            y -= 1f;
+
+        if (Input.GetKey(moveUp))
+            y += 1f;
+
+        MoveInput = new Vector2(x, y);
+
+        if (MoveInput.sqrMagnitude > 1f)
+            MoveInput = MoveInput.normalized;
+    }
+
+    private void ReadJump()
+    {
+        if (Input.GetKeyDown(jump))
+            JumpPressed = true;
+    }
+
+    public void ConsumeJump()
+    {
+        JumpPressed = false;
+    }
+
+    public void ResetInput()
+    {
+        MoveInput = Vector2.zero;
+        JumpPressed = false;
     }
 }
